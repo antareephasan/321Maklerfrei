cf87332d7aca449f6adeac9536eaa91c6e3cad98
antareephasan <antareephasan@gmail.com>
2024-10-30 19:46:45 +0600

    g

---
+++ client/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const app = express();
+app.use(express.static(path.join(dirname, 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port 167.99.52.98:${PORT}`);
+});
